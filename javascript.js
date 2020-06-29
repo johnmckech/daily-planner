8adf725096b0846885aa952f016bc17383336b32
John McK <john.mckech3@gmail.com>
2020-06-28 23:35:49 -0500

    adding to javascript

---
+++ javascript.js
@@ -0,0 +1,25 @@
+var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+var displayDay = document.getElementById('currentDay');
+displayDay.textContent = currentDay
+
+var pastTime = currentDay--
+var futureTime = currentDay++
+
+
+var createTask = function (event) {
+    event.preventDefault();
+    var taskNameEl = document.querySelector
+};
+
+//make current time variable, then
+
+
+function calendarColor() {
+    if 
+} 
+
+calendarColor();
+createTask;
+    
+
+ 
